2ea538047d90037779eada7f01acd9ff99873c74
Prince Verma <16836599+vprince001@users.noreply.github.com>
2019-06-19 19:08:38 +0530

    working with fs.readFileSync

--- to_json.js
+++ to_json.js
@@ -1,45 +1,47 @@
 const fs = require("fs");
-const readline = require("readline");
-let finalResult = [];
-let isFirstLine = true;
-let headers = [];
-let startPoints = [0];
-const readStream = fs.createReadStream("./data_file.txt");
 
-const readInterface = readline.createInterface({
-  input: readStream
-});
+const getStartPoints = function(firstLine) {
+  let headers = firstLine.split(" ");
+  let count = 0;
+  let startPoints = [0];
 
-readInterface
-  .on("line", function(line) {
-    if (isFirstLine) {
-      headers = line.split(" ");
-      let count = 0;
-      for (let index = 0; index < headers.length; index++) {
-        let element = headers[index];
-        if (element == "") {
-          count++;
-          if (headers[index + 1] != "") {
-            count++;
-            startPoints.push(count);
-          }
-        } else {
-          count += element.length;
-        }
+  for (let index = 0; index < headers.length; index++) {
+    let header = headers[index];
+    if (header == "") {
+      count++;
+      if (headers[index + 1] != "") {
+        count++;
+        startPoints.push(count);
       }
-      headers = headers.filter(element => element);
-      isFirstLine = false;
     } else {
-      let obj = {};
-      headers.forEach((element, index) => {
-        obj[element] = line
-          .substring(startPoints[index], startPoints[index + 1])
-          .trim();
-      });
-      finalResult.push(obj);
+      count += header.length;
     }
-  })
-  .on("close", function() {
-    fs.writeFileSync("./output.json", JSON.stringify(finalResult));
-    readInterface.close();
-  });
+  }
+  return startPoints;
+};
+
+const formatDataInArray = function(data, startPoints) {
+  let headers = data[0].split(" ").filter(element => element);
+  headers.pop();
+  let obj = {};
+  let finalResult = [];
+
+  for (let dataIndex = 1; dataIndex < data.length - 1; dataIndex++) {
+    for (let headerIndex = 0; headerIndex < headers.length; headerIndex++) {
+      obj[headers[headerIndex]] = data[dataIndex]
+        .substring(startPoints[headerIndex], startPoints[headerIndex + 1])
+        .trim();
+    }
+    finalResult.push(obj);
+  }
+  return finalResult;
+};
+
+const main = function() {
+  const data = fs.readFileSync("./data_file.txt", "utf8").split("\n");
+  const startPoints = getStartPoints(data[0]);
+  const finalResult = formatDataInArray(data, startPoints);
+  fs.writeFileSync("./output.json", JSON.stringify(finalResult));
+};
+
+main();
